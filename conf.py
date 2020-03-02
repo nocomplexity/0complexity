3969b80cdae2464bfcdfcc71b1f3b718e97cf8dd
Maikel Mardjan <mike@bm-support.org>
2020-03-02 14:17:58 +0100

    rtd error fix

--- conf.py
+++ conf.py
@@ -99,7 +99,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
 #import sphinx_rtd_theme
 
 #html_theme = "sphinx_rtd_theme"
